6c63d22649d2cf5fb3fbbda000f2b0155196e533
Shailesh Kanzariya <shailesh.kanzariya@infostretch.com>
2016-06-15 17:51:24 +0530

    modified configs

--- config.js
+++ config.js
@@ -5,8 +5,9 @@ module.exports.settings = {
     tokenUrl: 'https://login.microsoftonline.com/24552a45-a658-456f-8ee1-0a5eb8f08b53/oauth2/token',
     clientId: '28ca4177-9ea6-4d4f-ab13-cb2d7d91d31e',
     clientSecret: 'TGTG/TmUDuxPO0QrUrI71rrl/0qzSPxWDke7bXqpIqo=',
-    callbackUrl: 'https://node-azuread-oauth-sample-kanzariya-shailesh.c9users.io/auth/waad/callback',
+    callbackUrl: 'https://azureadnodesample.azurewebsites.net/auth/waad/callback',
     resource: 'https://graph.windows.net',
     //mongodb: 'mongodb://admin:admin@ds038739.mlab.com:38739/hrdb'
-    mongodb:'mongodb://node-azuread-oauth-sample-kanzariya-shailesh.c9users.io/myapp'
+    mongodb:'mongodb://https://azureadnodesample.azurewebsites.net/myapp'
+    
 };
